273fd13d499a32a3fedfe3c0392f50193dfab196
조인식 <imsif42@naver.com>
2024-02-13 18:52:23 +0900

    fix: useQuery 적용

--- workoutLog/src/components/Community/WorkoutBox.tsx
+++ workoutLog/src/components/Community/WorkoutBox.tsx
@@ -14,50 +14,22 @@ import {
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import ShareIcon from '@mui/icons-material/Share';
-import { useEffect, useState } from 'react';
-import { deleteDoc, doc, onSnapshot, updateDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { deleteDoc, doc, getDoc, updateDoc } from 'firebase/firestore';
 import HeartButton from './HeartButton';
-
-interface IRecord {
-  category?: number;
-  content?: string;
-  createdAt?: number;
-  photoUrl?: string;
-  profileUrl?: string;
-  userId?: string;
-  username?: string;
-  like?: number;
-}
+import { useMutation, useQuery } from '@tanstack/react-query';
 
 export default function WorkoutBox() {
   const navigate = useNavigate();
   const user = auth.currentUser;
   const { recordId } = useParams();
-  const [record, setRecord] = useState<IRecord>({});
 
   const [like, setLike] = useState(false);
   if (recordId === undefined) return;
   const recordRef = doc(db, 'workoutrecords', recordId);
   const fetchRecord = async () => {
-    // if (recordId === undefined) return;
-    // const recordRef = doc(db, 'workoutrecords', recordId);
-    onSnapshot(recordRef, (snapshot) => {
-      if (snapshot.exists()) {
-        setRecord(snapshot.data());
-      }
-    });
-  };
-  console.log(record);
-  useEffect(() => {
-    fetchRecord();
-  }, []);
-  console.log(like);
-  const toggleLike = async () => {
-    setLike(!like);
-    //localstorage에 like 상태 유지? react query 사용해서 구현
-    if (record.like) {
-      await updateDoc(recordRef, { like: record.like + 1 });
-    }
+    const snapshot = await getDoc(recordRef);
+    return snapshot.data();
   };
   //본인 기록 삭제
   const handleDelete = async () => {
@@ -68,6 +40,33 @@ export default function WorkoutBox() {
       console.log(error);
     }
   };
+  //좋아요 기능
+  const toggleLike = async () => {
+    setLike(!like);
+
+    if (data?.like) {
+      await updateDoc(recordRef, { like: data?.like + 1 });
+    }
+  };
+  const updateMutation = useMutation({
+    mutationFn: () => toggleLike(),
+  });
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ['record'],
+    queryFn: fetchRecord,
+  });
+  const deleteMutation = useMutation({
+    mutationFn: () => handleDelete(),
+  });
+
+  if (isLoading) {
+    return <h1>loading..</h1>;
+  }
+  if (isError) {
+    console.log(error);
+    return <h1>error</h1>;
+  }
 
   return (
     <Card sx={{ maxWidth: 600 }}>
@@ -76,37 +75,39 @@ export default function WorkoutBox() {
           <Avatar
             sx={{ bgcolor: 'red[500]' }}
             aria-label="recipe"
-            src={record.profileUrl}
+            src={data?.profileUrl}
           />
         }
         action={
-          user?.uid === record.userId ? (
+          user?.uid === data?.userId ? (
             <Tooltip title="Delete">
-              <IconButton onClick={handleDelete}>
+              <IconButton onClick={() => deleteMutation.mutate()}>
                 <DeleteIcon />
               </IconButton>
             </Tooltip>
           ) : null
         }
-        title={record.username}
+        title={data?.username}
       />
       <CardMedia
         component="img"
         height="194"
-        image={record.photoUrl}
+        image={data?.photoUrl}
         alt="Paella dish"
       />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
-          {record.content}
+          {data?.content}
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <HeartButton click={toggleLike} like={like} />
+        <HeartButton click={() => updateMutation.mutate()} like={like} />
+
         <IconButton aria-label="share">
           <ShareIcon />
         </IconButton>
       </CardActions>
+      <span>{`${data?.like}`}</span>
     </Card>
   );
 }
